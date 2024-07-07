bddf7128af126c5dc67be41a5b86ced9c3940d07
bshar12ba12 <139974264+bshar12ba12@users.noreply.github.com>
2024-07-07 21:38:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const names = [
+    "دهام احمد خلف",
+   "روز فؤاد ابراهيم",
+    "سرهلدان محمود مصطفى ",
+    "شيخموس حسن شيخو",
+    "عبدالعزيز حسين شيخموس",
+    "عمر الحمد المسواط",
+    "محمدصالح عمر علي",
+    "حميده محمد علي عيسى",
+    "حميده محمدعلي عيسى",
+    "مراد فواز معمي",
+    // أضف الأسماء المخزنة هنا
+];
+
+function checkName() {
+    const nameInput = document.getElementById('nameInput').value.trim();
+    const messageDiv = document.getElementById('message');
+
+    if (names.includes(nameInput)) {
+        messageDiv.textContent = "نعم هنالك حوالة با اسمك يرجى مراجعة شركة رسيني الدرباسية منتصف السوق يرجى احضار الهوية وصاحب العلاقة شخصيا";
+        messageDiv.className = "message success";
+    } else {
+        messageDiv.textContent = "ناسف لكم قد تكون الحوالة الخاصة بك غير موجودة او لم تصل بعد يرجى عليكم الاتصال بالمرسل والتاكد من الشركة المرسلة وشكرا";
+        messageDiv.className = "message error";
+    }
+}
